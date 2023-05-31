36e48e371923a28fc06be7df3fabadbd23d9aa6b
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-05-31 15:40:45 -0300

    games

--- web/app/queue/page.tsx
+++ web/app/queue/page.tsx
@@ -9,81 +9,81 @@ import { useEffect, useState } from "react";
 import { FriendsList } from "@/Components/Friends/FriendsComponents";
 
 export default function QueueHoldingPage({
-  searchParams,
+     searchParams,
 }: {
-  searchParams: any;
+     searchParams: any;
 }) {
-  const { user, friends: userFriends, getFriends } = useUser();
+     const { user, friends: userFriends, getFriends } = useUser();
 
-  useEffect(() => {
-    getFriends();
-  }, []);
+     useEffect(() => {
+          getFriends();
+     }, []);
 
-  const [gameName, _] = useState<GameType[]>(
-    searchParams.games
-      ? searchParams.games
-          .split(",")
-          .map((game: GameType) => getGameData(Number(game)))
-      : []
-  );
-  const [timer, setTimer] = useState<NodeJS.Timer | undefined>(undefined);
+     const [games, _] = useState<GameType[]>(
+          searchParams.games
+               ? searchParams.games
+                      .split(",")
+                      .map((game: GameType) => getGameData(Number(game)))
+               : []
+     );
+     const [timer, setTimer] = useState<NodeJS.Timer | undefined>(undefined);
 
-  useEffect(() => {
-    if (timer) {
-      clearInterval(timer);
-    } else {
-      const timerId = setInterval(() => {
-        // queueSocket.emit("get_state", (data) => {});
-      }, 10000);
-      setTimer(timerId);
-    }
-  }, []);
+     useEffect(() => {
+          if (timer) {
+               clearInterval(timer);
+          } else {
+               const timerId = setInterval(() => {
+                    // queueSocket.emit("get_state", (data) => {});
+               }, 10000);
+               setTimer(timerId);
+          }
+     }, []);
 
-  useEffect(() => {
-    if (queueSocket.connected) return;
-    queueSocket.auth = {
-      user: user,
-    };
-    queueSocket.connect();
-    queueSocket.emit("join_queue", gameName);
-
-    queueSocket.on("game_found", (data: any) => {
-      window.location.href = `/play/${data}`;
-    });
-    return () => {
-      if (queueSocket.connected) {
-        queueSocket.disconnect();
-      }
-    };
-  }, []);
-  return (
-    <div
-      className="flex flex-row
+     useEffect(() => {
+          if (queueSocket.connected) return;
+          queueSocket.auth = {
+               user: user,
+          };
+          queueSocket.connect();
+          console.log(games);
+          queueSocket.emit("join_queue", games);
+          queueSocket.on("game_found", (data: any) => {
+               window.location.href = `/play/${data}`;
+          });
+          return () => {
+               if (queueSocket.connected) {
+                    queueSocket.disconnect();
+               }
+          };
+     }, []);
+     return (
+          <div
+               className="flex flex-row
     "
-    >
-      <div className="m-auto pt-12 w-fit">
-        <Heading>Currently Finding a Game</Heading>
-        <ul className="grid-flow-col py-10 grid">
-          {gameName.map((gameData, index) => {
-            return (
-              <Card className="w-fit p-6 " key={index}>
-                <Text>{gameData.name}</Text>
-              </Card>
-            );
-          })}
-        </ul>
-        <div className="pb-6 text-center">
-          <Text className="text-xl">
-            You have been waiting for <span>12 minutes</span>
-          </Text>
-        </div>
-        <div className="text-center text-2xl">
-          Currently there are <span>0</span> people in queue.
-        </div>
-      </div>
-      <div>
-        <FriendsList friends={userFriends} />
-      </div>
-    </div>
-  );
+          >
+               <div className="m-auto pt-12 w-fit">
+                    <Heading>Currently Finding a Game</Heading>
+                    <ul className="grid-flow-col py-10 grid">
+                         {games.map((gameData, index) => {
+                              return (
+                                   <Card className="w-fit p-6 " key={index}>
+                                        <Text>{gameData.name}</Text>
+                                   </Card>
+                              );
+                         })}
+                    </ul>
+                    <div className="pb-6 text-center">
+                         <Text className="text-xl">
+                              You have been waiting for <span>12 minutes</span>
+                         </Text>
+                    </div>
+                    <div className="text-center text-2xl">
+                         Currently there are <span>0</span> people in queue.
+                    </div>
+               </div>
+               <div>
+                    <FriendsList friends={userFriends} />
+               </div>
+          </div>
+     );
 }
