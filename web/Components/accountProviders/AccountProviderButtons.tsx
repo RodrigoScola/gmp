f3449f0747ef4d613d2411d9276f72598efe9255
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-05-31 11:08:18 -0300

    account provider buttons

---
+++ web/Components/accountProviders/AccountProviderButtons.tsx
@@ -0,0 +1,34 @@
+import { BsDiscord, BsGithub, BsGoogle } from "react-icons/bs";
+
+export const AccountProviders = {
+     discord: {
+          color: "bg-blue-500",
+          Icon: BsDiscord,
+     },
+     github: {
+          color: "bg-slate-500",
+          Icon: BsGithub,
+     },
+     google: {
+          color: "bg-red-500",
+          Icon: BsGoogle,
+     },
+} as const;
+export type AccountProviderType = keyof typeof AccountProviders;
+export const ProviderButton = ({
+     handleClick,
+     provider,
+}: {
+     provider: AccountProviderType;
+     handleClick: (provider: AccountProviderType) => Promise<void>;
+}) => {
+     const { color, Icon } = AccountProviders[provider];
+     return (
+          <div
+               onClick={() => handleClick(provider)}
+               className={`${color} hover:cursor-pointer rounded-md flex py-2 justify-center`}
+          >
+               <Icon />
+          </div>
+     );
+};
