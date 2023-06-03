196736d9487ba304968fae1ae4a95ace73ae6ed0
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-06-03 16:22:35 -0300

    asdf

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,11 +3,11 @@ import { FriendsList } from "@/Components/Friends/FriendsComponents";
 import { useFriends } from "@/hooks/useFriends";
 import { useUser } from "@/hooks/useUser";
 // import { getUrl } from "@/lib/utils";
-import { IFriend } from "../../shared/src/types/users";
+import { AddNewFriend } from "@/Components/Friends/AddNewFriend";
 import Link from "next/link";
 import { useState } from "react";
-import { useEffectOnce, useUpdateEffect } from "usehooks-ts";
-import { AddNewFriend } from "@/Components/Friends/AddNewFriend";
+import { useEffectOnce } from "usehooks-ts";
+import { IFriend } from "../../shared/src/types/users";
 export default function Home() {
      const { user, isLoggedIn } = useUser();
      const [userFriends, setFriends] = useState<IFriend[]>([]);
