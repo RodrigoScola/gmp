aaa650f910876e4f66f50c504abd4bd5b69a14dd
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-06-05 11:00:10 -0300

    asdfs

--- web/hooks/useUser.tsx
+++ web/hooks/useUser.tsx
@@ -64,7 +64,7 @@ export const UserProvider = ({ children }: { children: ChildrenType }) => {
      useEffect(() => {
           handleFetch();
      }, [session]);
-     useUpdateEffect(() => {
+     useEffect(() => {
           if (userSocket.connected) return;
           userSocket.auth = {
                user: currentUser,
