8e02ec11482aa4e4c7e27cd19ffd90ea7c3d049a
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-06-05 14:16:06 -0300

    re

--- web/hooks/useUser.tsx
+++ web/hooks/useUser.tsx
@@ -61,6 +61,7 @@ export const UserProvider = ({ children }: { children: ChildrenType }) => {
                     setCurrentUser(data.data);
                }
           }
+     }
           useEffect(() => {
                handleFetch();
           }, [session]);
