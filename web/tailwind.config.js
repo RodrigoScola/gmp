a8bdc5021e8f74c5185dbe409d7d64a4f3e88647
RodrigoScola <rodrigo.sgarabotto.scola@gmail.com>
2023-06-21 11:49:46 -0300

    Compoent

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,13 +5,7 @@ module.exports = {
      content: [
           "./app/**/*.{js,ts,jsx,tsx}",
           "./pages/**/*.{js,ts,jsx,tsx}",
-          "./components/**/*.{js,ts,jsx,tsx}",
-          // Or if using `src` directory:
-          "./src/**/*.{js,ts,jsx,tsx}",
-     ],
-     purge: [
-          "./src/**/*.{js,ts,jsx,tsx}",
-          // Add more here
+          "./Components/**/*.{js,ts,jsx,tsx}",
      ],
      theme: {
           colors: {
